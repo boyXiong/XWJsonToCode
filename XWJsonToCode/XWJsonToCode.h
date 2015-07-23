30d00fe7762988a47be9a8289030a503169f790f
boyXiong <relv@qq.com>
2015-07-23 09:53:00 +0800

    01-初步可以运行的插件

---
+++ XWJsonToCode/XWJsonToCode.h
@@ -0,0 +1,17 @@
+//
+//  XWJsonToCode.h
+//  XWJsonToCode
+//
+//  Created by key on 15/7/23.
+//  Copyright (c) 2015年 熊  伟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+@interface XWJsonToCode : NSObject
+
+@property (nonatomic,copy) NSString *selectedText;
+
+
+@end
---
+++ XWJsonToCode/XWJsonToCode.m
@@ -0,0 +1,79 @@
+//
+//  XWJsonToCode.m
+//  XWJsonToCode
+//
+//  Created by key on 15/7/23.
+//  Copyright (c) 2015年 熊  伟. All rights reserved.
+//
+
+#import "XWJsonToCode.h"
+
+@implementation XWJsonToCode
+
++ (void) pluginDidLoad: (NSBundle*) plugin {
+    [self shared];
+}
+
+
++(id) shared {
+
+    static id instance = nil;
+
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+
+        instance = [[self alloc] init];
+    });
+    return instance;
+}
+
+- (id)init {
+    if (self = [super init]) {
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(applicationDidFinishLaunching:)
+                                                     name:NSApplicationDidFinishLaunchingNotification
+                                                   object:nil];
+    }
+    return self;
+}
+
+- (void) applicationDidFinishLaunching: (NSNotification*) noti {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(selectionDidChange:)
+                                                 name:NSTextViewDidChangeSelectionNotification
+                                               object:nil];
+    NSMenuItem *editMenuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
+    if (editMenuItem) {
+        [[editMenuItem submenu] addItem:[NSMenuItem separatorItem]];
+        NSMenuItem *newMenuItem = [[NSMenuItem alloc] initWithTitle:@"What is selected" action:@selector(showSelected:) keyEquivalent:@""];
+        [newMenuItem setTarget:self];
+        [newMenuItem setKeyEquivalentModifierMask: NSAlternateKeyMask];
+        [[editMenuItem submenu] addItem:newMenuItem];
+    }
+}
+
+
+-(void) selectionDidChange:(NSNotification *)noti {
+    if ([[noti object] isKindOfClass:[NSTextView class]]) {
+        NSTextView* textView = (NSTextView *)[noti object];
+
+        NSArray* selectedRanges = [textView selectedRanges];
+        if (selectedRanges.count==0) {
+            return;
+        }
+
+        NSRange selectedRange = [[selectedRanges objectAtIndex:0] rangeValue];
+        NSString* text = textView.textStorage.string;
+        self.selectedText = [text substringWithRange:selectedRange];
+    }
+    //Hello, welcom to OneV's Den
+}
+
+-(void) showSelected:(NSNotification *)noti {
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert setMessageText: self.selectedText];
+    [alert runModal];
+}
+
+
+@end
